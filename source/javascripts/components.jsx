8f9181eaf113c0fb8739fe4143dfc8860008f9aa
Laurie Young <laurie@wildfalcon.com>
2014-10-24 13:25:13 +0100

    Added comments, and formatted them using Markdown

--- source/javascripts/components.jsx
+++ source/javascripts/components.jsx
@@ -5,7 +5,46 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-      Hello, world! I am a CommentBox.
+       <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+         <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+
+var converter = new Showdown.converter();
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
       </div>
     );
   }
